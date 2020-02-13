284ba24ac1c99d9213a2e90eed30916fec3a18f7
KK980827 <KK980827@163.com>
2020-02-13 17:28:03 +0800

    Mybatis Plus Auto Generator Init

---
+++ src/test/java/com/bestbigkk/CodeGenerator.java
@@ -0,0 +1,132 @@
+package com.bestbigkk;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class CodeGenerator {
+
+    private static final String TEMPLATE_DIR = "/mybatisplus/templates";
+    private static final String MAPPER_DIR = "/src/main/resources/mapper";
+    private static final String AUTHOR = "xugongkai";
+    public static final String DB_URL = "";
+    public static final String DB_DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String DB_USER_NAME = "";
+    public static final String DB_USER_PASSWORD = "";
+    public static final String DB_SCHEMA_NAME = null;
+    public static final String PACKAGE_NAME = "com.bestbigkk";
+    public static final String TABLE_PREFIX = "t_";
+
+
+    private static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor(AUTHOR);
+        gc.setOpen(false);
+        //实体名称，%s为占位符
+        gc.setEntityName("%sPO");
+        //开启Swagger注释
+        gc.setSwagger2(true);
+        //是否覆盖文件
+        gc.setFileOverride(true);
+        //自定义文件名
+        gc.setMapperName("%sMapper");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        //实体属性 Swagger2 注解
+         gc.setSwagger2(true);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl(DB_URL);
+        dsc.setDriverName(DB_DRIVER_CLASS_NAME);
+        dsc.setUsername(DB_USER_NAME);
+        dsc.setPassword(DB_USER_PASSWORD);
+        if (!Objects.isNull(DB_SCHEMA_NAME)) {
+            dsc.setSchemaName(DB_SCHEMA_NAME);
+        }
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(scanner("模块名"));
+        pc.setParent(PACKAGE_NAME);
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = TEMPLATE_DIR + "/mapper.xml.ftl";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + MAPPER_DIR + File.separator  + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(TABLE_PREFIX);
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
